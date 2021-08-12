507c214d9c1df19aad54dde0d7250762b1097091
ikizey <ikizey@icloud.com>
2021-08-12 22:16:50 +0300

    initial

---
+++ VK App/ViewController.swift
@@ -0,0 +1,16 @@
+//  Created by ikizey on 12.08.2021.
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
