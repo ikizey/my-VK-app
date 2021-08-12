f99209e71e47121bd124115ed966a497455af9ab
ikizey <ikizey@icloud.com>
2021-08-12 23:29:15 +0300

    add outlets and actions

--- VK App/LoginViewController.swift
+++ VK App/LoginViewController.swift
@@ -6,11 +6,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton! // for later
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    private func isValidLogin() -> Bool {
+        !loginTextField.text!.isEmpty
+    }
 
+    private func isValidPassword() -> Bool {
+        passwordTextField.text!.count > 3
+    }
+
+    @IBAction func login() {
+        guard isValidLogin(), isValidPassword() else {
+            let alert = UIAlertController(title: "Error",
+                                          message: "login or password is too short",
+                                          preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+            return
+        }
+
+        //
+    }
 }
 
